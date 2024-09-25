f6294d055de854648dd52689a96c22693e938be4
5ma111eaf <172279142+5ma111eaf@users.noreply.github.com>
2024-09-25 15:06:12 +0800

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -4,10 +4,18 @@
 # 更新软件包列表
 sudo apt update
 
-# 安装 nmap
+# 安装 Nmap
 sudo apt install -y nmap
 
-# 安装 masscan
+# 安装 Masscan
 sudo apt install -y masscan
+
+# 安装 CrackMapExec
+sudo snap install crackmapexec
+
+# 安装 httpx
+sudo snap install httpx
+
+# 安装 
 
 echo "安装完成！"
